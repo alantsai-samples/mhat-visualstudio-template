7fb12361a663a2eba4d0f7242ee810e468af7761
Alan Tsai <alan@alantsai.net>
2017-09-02 10:31:20 +0800

    加入單個專案匯出的template

---
+++ src/VSIXSingProjectTemplate/VSIXSingProjectTemplate/Process/EchoProcess.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MHAT.ConsoleApp.ProcessTemplate;
+using $safeprojectname$.Model;
+
+namespace $safeprojectname$.Process
+{
+    public class EchoProcess : BaseExecuteProcessTemplate<EchoProcessOption>
+    {
+        protected override void PreProcess()
+        {
+            Console.WriteLine("準備輸出傳入的參數.....");
+            base.PreProcess();
+        }
+
+        protected override void Process()
+        {
+            Console.WriteLine($"您輸入的是：{ArugemntOption.Text}");
+            base.Process();
+        }
+
+        protected override void PostProcess()
+        {
+            Console.WriteLine("完成輸出輸入的參數");
+            Console.WriteLine("程式關閉");
+            base.PostProcess();
+        }
+    }
+}
