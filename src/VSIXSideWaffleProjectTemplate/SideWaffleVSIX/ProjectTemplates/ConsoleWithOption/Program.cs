45f3dd7a5d0d0090c9f967793a95643ac0a73d85
Alan Tsai <alan@alantsai.net>
2017-09-11 23:30:53 +0800

    convention 的方式建立Project Template

---
+++ src/VSIXSideWaffleProjectTemplate/SideWaffleVSIX/ProjectTemplates/ConsoleWithOption/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MHAT.ConsoleWithOption.Process;
+
+namespace MHAT.ConsoleWithOption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var process = new EchoProcess();
+            process.StartProcess(args);
+        }
+    }
+}
